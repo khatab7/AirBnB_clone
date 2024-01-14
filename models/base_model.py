369c87d49c7c8ce1c2ba040a56643e586f63a0da
maazalside <maazalside@gmail.com>
2024-01-14 20:17:48 +0300

    decument test class

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,6 @@ import datetime
 
 class BaseModel:
     """Base Class for all other Classes"""
-
     def __init__(self):
         """Set up a new instance with a unique ID and timestamps."""
         self.id = str(uuid.uuid4())
@@ -19,14 +18,15 @@ class BaseModel:
 
     def save(self):
         """Save the current state of the object."""
-        update_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """Convert the object into a dictionary for storage or sharing."""
-        obj = self.__dict__.copy()
+        dictionary = {}
 
-        obj['__class__'] = self.__class__.__name__
-
-        obj['created_at'] = self.created_at.isoformat()
-        obj['updated_at'] = self.updated_at.isoformat()
-        return obj
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""TestBaseModel test for BaseModel class."""
 import unittest
 from unittest.mock import patch, Mock
 from models.base_model import BaseModel
@@ -7,9 +9,10 @@ mock_time = datetime.datetime(2024, 1, 11, 10, 19, 18, 818094)
 
 
 class TestBaseModel(unittest.TestCase):
-
+    """Unit tests for the BaseModel class."""
     @classmethod
     def setUpClass(cls):
+        """Setup class method to patch the time function."""
         cls.mock_time_patch = patch(
             'time.time',
             return_value=mock_time.timestamp())
@@ -17,21 +20,26 @@ class TestBaseModel(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        """Teardown class method to stop the time patch."""
         cls.mock_time_patch.stop()
 
     def setUp(self):
+        """Setup method to create a BaseModel instance."""
         self.base_model = BaseModel()
 
     def test_init(self):
+        """Test case to check the initialization of BaseModel attributes."""
         self.assertIsNotNone(self.base_model.id)
         self.assertIsInstance(self.base_model.created_at, datetime.datetime)
         self.assertIsInstance(self.base_model.updated_at, datetime.datetime)
 
     def test_str(self):
+        """Test case to check the string representation of BaseModel."""
         out = f"[BaseModel] ({self.base_model.id}) {self.base_model.__dict__}"
         self.assertEqual(str(self.base_model), out)
 
     def test_save(self):
+        """Test case to check the save method of BaseModel."""
         mock_now = datetime.datetime(2024, 1, 11, 10, 19, 18, 818094)
 
         with patch('datetime.datetime') as mock_datetime:
@@ -43,6 +51,7 @@ class TestBaseModel(unittest.TestCase):
             self.assertGreaterEqual(self.base_model.updated_at, ini_upd)
 
     def test_to_dict(self):
+        """Test case to check the to_dict method of BaseModel."""
         result = self.base_model.to_dict()
 
         self.assertIn('__class__', result)
