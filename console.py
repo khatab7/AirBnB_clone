f411813e71574d7bef84b63b0a81897c7b351c03
maazalside <maazalside@gmail.com>
2024-01-15 09:05:13 +0300

    fix console.py to print id of create class

--- console.py
+++ console.py
@@ -36,6 +36,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         else:
             new_instance = BaseModel()
+            print(new_instance.id)
             new_instance.save()
 
     def do_show(self, arg):
