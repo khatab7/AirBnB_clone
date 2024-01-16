5d746231659e257502e491a187b2c8ac60ccc9de
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 <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>
2024-01-16 15:45:14 -0800

    test filestoeage update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,56 +1,77 @@
 #!/usr/bin/python3
-"""This module defines a class FileStorage"""
+"""
+Module for serializing and deserializing data
+"""
 import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file
-    to instances"""
+    """
+    FileStorage class for storing, serializing and deserializing data
+    """
     __file_path = "file.json"
+
     __objects = {}
 
-    def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
-
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        """
+         Sets an object in the __objects dictionary with a key of 
+         <obj class name>.id.
+        """
+        obj_cls_name = obj.__class__.__name__
+
+        key = "{}.{}".format(obj_cls_name, obj.id)
+
+        FileStorage.__objects[key] = obj
+
+
+    def all(self):
+        """
+        Returns the __objects dictionary. 
+        It provides access to all the stored objects.
+        """
+        return  FileStorage.__objects
+
 
     def save(self):
-        """ """
-        with open(FileStorage.__file_path, 'w') as file:
-            data = {}
-            data.update(FileStorage.__objects)
-            for key, value in data.items():
-                data[key] = value.to_dict()
-            json.dump(data, file)
+        """
+        Serializes the __objects dictionary into 
+        JSON format and saves it to the file specified by __file_path.
+        """
+        all_objs = FileStorage.__objects
+
+        obj_dict = {}
+
+        for obj in all_objs.keys():
+            obj_dict[obj] = all_objs[obj].to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.amenity import Amenity
-        from models.city import City
-        from models.review import Review
-        from models.state import State
+        """
+        This method deserializes the JSON file
+        """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                try:
+                    obj_dict = json.load(file)
 
-        
-        cls_name = {
-            'BaseModel': BaseModel,
-            'User': User,
-            'Place': Place,
-            'Amenity': Amenity,
-            'City': City,
-            'Review': Review,
-            'State': State
-            }
-        try:
-            data = {}
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    self.all()[key] = cls_name[value['__class__']](**value)
-        except FileNotFoundError:
-            pass
+                    for key, value in obj_dict.items():
+                        class_name, obj_id = key.split('.')
+
+                        cls = eval(class_name)
+
+                        instance = cls(**value)
+
+                        FileStorage.__objects[key] = instance
+                except Exception:
+                    pass
