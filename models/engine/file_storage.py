5169a4a1e5990e72521432d01750ac68e3e3187e
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 <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>
2024-01-16 18:52:41 -0800

    fix code add new feture

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,6 +26,36 @@ class FileStorage:
                 data[key] = value.to_dict()
             json.dump(data, file)
 
+    def update(self, obj_id, attr_key, attr_value):
+        try:
+            attr_value = self.__clean_quotes(attr_value)
+            if attr_key not in ["id", "created_at", "updated_at"]:
+                self.__objects[f'{obj_id}'].__dict__[
+                    f'{attr_key}'] = attr_value
+                self.save()
+                return True
+            else:
+                return False
+        except KeyError:
+            return False
+
+    def __clean_quotes(self, to_clean):
+        clean_attr_value = to_clean.split("\"")
+        if len(clean_attr_value) > 1:
+            return clean_attr_value[1]
+        else:
+            return clean_attr_value[0]
+
+    def remove(self, obj_id):
+        '''Public instance method to delete a
+         certain element from the dictionary'''
+        try:
+            del self.__objects[f'{obj_id}']
+            self.save()
+            return True
+        except KeyError:
+            return False
+
     def reload(self):
         """deserializes the JSON file to __objects"""
         from models.base_model import BaseModel
